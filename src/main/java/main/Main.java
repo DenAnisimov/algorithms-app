509bf9db792017f15c92f68b76bb85df3199846e
Denis Anisimov <den.anisimov.94@mail.ru>
2023-12-07 22:48:26 +0300

    Test of simple search

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,20 @@
 package main;
 
+import main.service.Algorithms;
+
 public class Main {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Algorithms.simpleSearch(generateMassive(999_999_999), 999_999_998);
+        long end = System.currentTimeMillis() - start;
+        System.out.println("Binary search milliseconds: " + end);
+    }
 
+    public static int[] generateMassive(int size) {
+        int[] mass = new int[size];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = i;
+        }
+        return mass;
     }
 }
