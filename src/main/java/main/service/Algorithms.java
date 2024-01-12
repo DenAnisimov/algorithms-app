aba195a4f8d94c3fcc67ebd8d0a74a2a4cdb27a0
Denis Anisimov <den.anisimov.94@mail.ru>
2024-01-12 20:38:25 +0300

    Binary search

--- src/main/java/main/service/Algorithms.java
+++ src/main/java/main/service/Algorithms.java
@@ -15,4 +15,29 @@ public class Algorithms {
         }
         return index;
     }
+
+    /**
+     * @param numbers massive of data
+     * @param target target number
+     * @return index of number in massive or -1 if target not found*/
+    public static int binarySearch(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+        int middle;
+        int guess;
+        while (low <= high) {
+            middle = (low + high) / 2;
+            guess = numbers[middle];
+            if (guess == target) {
+                return middle;
+            }
+            if (guess > target) {
+                high = middle - 1;
+            }
+            if (guess < target) {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
 }
