b930b0586bf096b7a34f9dbf526074d937eeb08c
Denis Anisimov <den.anisimov.94@mail.ru>
2024-01-12 21:40:38 +0300

    Fixed a typo and add generateReverseArray method

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,25 +1,40 @@
 package main;
 
 import main.service.Algorithms;
+import main.service.Sortings;
 
 public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        Algorithms.simpleSearch(generateMassive(999_999_999), 999_999_998);
+        Algorithms.simpleSearch(generateArray(999_999_999), 999_999_998);
         long end = System.currentTimeMillis() - start;
         System.out.println("Simple search milliseconds: " + end);
 
         start = System.currentTimeMillis();
-        Algorithms.binarySearch(generateMassive(999_999_999), 999_999_998);
+        Algorithms.binarySearch(generateArray(999_999_999), 999_999_998);
         end = System.currentTimeMillis() - start;
         System.out.println("Binary search milliseconds: " + end);
+
+        start = System.currentTimeMillis();
+        Sortings.selectionSort(generateReverseArray(100_000));
+        end = System.currentTimeMillis() - start;
+        System.out.println("Selection sort milliseconds: " + end);
     }
 
-    public static int[] generateMassive(int size) {
+    public static int[] generateArray(int size) {
         int[] mass = new int[size];
         for (int i = 0; i < mass.length; i++) {
             mass[i] = i;
         }
         return mass;
+    }
+
+    public static int[] generateReverseArray(int size) {
+        int[] array = new int[size];
+        int arrayLength = size;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arrayLength--;
+        }
+        return array;
     }
 }
